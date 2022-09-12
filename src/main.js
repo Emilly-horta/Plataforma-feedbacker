1f1ff4eb68b59364b88a92abe927a58f67f37183
Emilly <millyphb@gmail.com>
2022-09-12 16:15:02 -0300

    add routers and font faces

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
+import 'animate.css'
 import '@/assets/css/tailwind.css'
 import '@/assets/css/fonts.css'
-import 'animate.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
